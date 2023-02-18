7fe6626a8170354229de7bf723c73b8746389592
Min1705 <ndnm1705@gmail.com>
2023-02-19 00:09:23 +0700

    update file

---
+++ test.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int b[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>b[i];
+    }
+    cout << " Hello " << a[0];
+    return 0;
+
+}
